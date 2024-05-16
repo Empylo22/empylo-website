5775c251f9464cd9a0d607e735b84a34f457acad
TechXpressSolution <abubakrsulaymaan@gmail.com>
2024-05-15 19:03:11 -0700

    Update wwa.js

--- src/components/whoweare/wwa.js
+++ src/components/whoweare/wwa.js
@@ -38,44 +38,20 @@ const Wwa =()=>{
                 <img className='wwa-1-2' src={img3} alt='img'/>
                 <div className='wwa-1-3'>
                     <p className='wwa-1-3-text'>
-                    Empylo Health is a technology and consulting company dedicated to tackling global 
-                    health challenges and their associated risks. Our innovative solutions leverage advanced data 
-                    analytics to empower individuals and organisations in building 
-                    meaningful connections and driving positive behavior change for improved health outcomes.
+                    Empylo Health is a software and consulting company dedicated to tackling global health challenges. 
+Research shows that loneliness and social isolation are linked to poor mental and physical health outcomes, as well as premature mortality.
                     </p>
                     <p className='wwa-1-3-text'>
-                    We believe that stronger connections lead to better health outcomes and 
-                    we have set out on a mission to tackle loneliness, and mental and physical health. 
-                    Research shows that loneliness and social isolation are linked to poor mental and 
-                    physical health outcomes, as well as premature mortality.  Our solutions are designed 
-                    to address the social, mental and physical health needs of both individuals and organisations, 
-                    offering tailored approaches to personal and group health management.
+                   We developed Circles Health App to empower societies to make insightful decisions and adopt healthier lifestyles.
                     </p>
                     <p className='wwa-1-3-text'>
-                    For individual consumers, we offer the Circles Health App, a user-friendly platform that empowers users to build stronger connections with their loved ones and take control of their health and wellbeing. Through features such as interactive dashboards with personalised and group insights, and community engagement tools, 
-                    the Circles Health App empowers users make insightful decisions and adopt healthier lifestyles.
+                    Circles Health App Client is a comprehensive solution designed to support a diverse range of organisational health and inclusion initiatives.
                     </p>
                     {
                       isClick ? (
                         <>
                         <p className='wwa-1-3-text'>
-                    For organisations, we offer the Circles Health App Client, a comprehensive solution designed 
-                    to support a diverse range of organisational health and wellbeing initiatives. With features 
-                    such as role-based access control, advanced assessment management, and integration support with 
-                    existing systems, the Circles Health App Client enables organisations to effectively manage and 
-                    promote the employee health and wellbeing, 
-                    make faster and insight-led decisions, and plan more effectively for the future workforce needs.
-                    </p>
-                    <p className='wwa-1-3-text'>
-                    As part of our mission to tackle loneliness and improve mental and physical, 
-                    we also offer training and consultancy 
-                    services for groups, communities and organisations to promote better connections and health outcomes. 
-                    </p>
-                    <p className='wwa-1-3-text'>
-                    Empylo Health is a clinician-led company committed to driving positive health outcomes through 
-                    extensive research and evidence, innovation, data analytics, and personalised approaches. 
-                    Whether you're an individual looking to improve your health or an organisation 
-                    seeking to support your teams, Empylo Health has the expertise and solutions to help you succeed. 
+                    As part of our mission to break social barrier and create safe environments, we also offer workshops and consultations for groups, communities and organisations to drive stronger connections and better health outcomes.
                     </p>
                         </>
                       ) : ('')
