af35b406beb29db38e3d9cfdedccd07f55efb6a8
TechXpressSolution <abubakrsulaymaan@gmail.com>
2024-05-16 04:13:54 +0100

    Update mission.js

--- src/components/mission/mission.js
+++ src/components/mission/mission.js
@@ -28,11 +28,7 @@ const Mission =()=>{
                 <h1 className='mission-header'>
                         About Us
                     </h1>
-                    <p className='mission-1-text'>We are on a mission is to enhance 
-                    lives through evidence-based digital health solutions that truly 
-                    revolutionise the way people live, thrive, and work. Rooted in 
-                    clinical expertise, we are a dynamic health technology and consulting company committed 
-                    to addressing global health challenges and their associated mental health and physical health risks.
+                    <p className='mission-1-text'>Rooted in clinical expertise, we are a dynamic software and consulting company committed to addressing global health challenges.
                     </p>
 
 
@@ -40,17 +36,10 @@ const Mission =()=>{
                         Our Mission
                     </h1>
                     <p className='mission-1-text'>
-                    Our mission is to improve lives through evidence-based digital health solutions. We want to build products that truly impact the way people live, thrive and work. We want to break social barriers and stigma associated with how people talk and share their social, psychological and physical health needs and outcomes. We want to create a platform where people freely communicate their health needs with those close to them 
-                    without feeling ashamed or scared. Our solutions help people to do that in a safe and nurturing way. 
+                   We want to build products that truly impact the way people live, thrive and work. We want to break social barriers and stigma associated with mental health and loneliness. Our solutions help people to communicate their health goals and outcomes in a safe and nurturing environment. 
                     </p>
                     <p className='mission-1-text'>
-                    We are a clinician led company with a focus on delivering evidence-based solutions that truly meet societal needs and promote global health equity. Our solutions use advanced data analytics to help people to build meaningful social connections 
-                    and gain insights to drive individual and group behaviour change and promote positive health outcomes.
-                    </p>
-                    <p className='mission-1-text'>
-                    We have designed innovative solutions for both individual consumers and organisations. 
-                    Our core offerings are the <span className='mission-span'>Circles Health App </span> 
-                    for personal use and <span className='mission-span'>Circles Health App Client</span> for organisations.
+                    We use evidence-based solutions to promote global health equity. Using advanced data analytics to gain insights that drive behaviour change and positive health outcomes.
                     </p>
                 </div>
                 <div className='mission-2'>
